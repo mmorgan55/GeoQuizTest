cf8e524b47b6bc40c7b8c8cccd38ce12a266315a
Matthew Morgan <mmorgan55@cnm.edu>
2018-10-11 18:37:43 -0600

    Added code to keep the state after the phone is rotated, disable the buttons after an answer is selected, and score tracker.

--- app/src/main/java/edu/cnm/deepdive/geoquiztest/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiztest/MainActivity.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.geoquiztest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "MainActivity";
+  private static final String KEY_INDEX = "index";
 
   private Button mTrueButton;
   private Button mFalseButton;
@@ -27,11 +31,21 @@ public class MainActivity extends AppCompatActivity {
   };
 
   private int mCurrentIndex = 0;
+  private int quizScore;
+  private int questionsAnswered;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_main);
+    quizScore = 0;
+    questionsAnswered = 0;
+
+    if (savedInstanceState != null) {
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
 
     mQuestionTextView = findViewById(R.id.question_text_view);
     mQuestionTextView.setOnClickListener(new OnClickListener() {
@@ -47,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         checkAnswer(true);
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
       }
     });
 
@@ -55,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         checkAnswer(false);
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
       }
     });
 
@@ -64,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
       public void onClick(View v) {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
         updateQuestion();
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
       }
     });
 
@@ -78,10 +98,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         updateQuestion();
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
       }
     });
 
     updateQuestion();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
   }
 
   private void updateQuestion() {
@@ -94,10 +154,19 @@ public class MainActivity extends AppCompatActivity {
     int messageResId;
     if (userPressedTrue == answerIsTrue) {
       messageResId = R.string.correct_toast;
+      quizScore++;
+      questionsAnswered++;
     } else {
       messageResId = R.string.incorrect_toast;
+      questionsAnswered++;
     }
 
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+    if (questionsAnswered == 6) {
+      Toast.makeText(this,
+                     "You got " + quizScore + "/" + questionsAnswered + " correct!",
+                     Toast.LENGTH_SHORT).show();
+    }
   }
 }
